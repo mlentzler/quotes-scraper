b582373f20deed2ac80163be386433df8c306e87
mlentzler <michel.lentzler@gmail.com>
2025-01-06 20:36:24 +0100

    formatting

--- internal/crawler/crawler.go
+++ internal/crawler/crawler.go
@@ -6,28 +6,26 @@ import (
 	"net/http"
 )
 
-
-
 func Crawl(link string) {
-  fmt.Printf("Crawling URL: %s\n", link)
+	fmt.Printf("Crawling URL: %s\n", link)
 }
 
-func Fetch (url string) (string, error) {
-  response, err := http.Get(url)
-  if err != nil {
-    return "", fmt.Errorf("An Error Occured while fetching: %v", err)
-  }
+func Fetch(url string) (string, error) {
+	response, err := http.Get(url)
+	if err != nil {
+		return "", fmt.Errorf("An Error Occured while fetching: %v", err)
+	}
 
-  defer response.Body.Close()
+	defer response.Body.Close()
 
-  if response.StatusCode != http.StatusOK {
-    return "", fmt.Errorf("Unexpected Status code: %d", response.StatusCode)
-  }
-  
-  bodyBytes, err := io.ReadAll(response.Body)
-  if err != nil {
-    return "", fmt.Errorf("An Error Occured while reading the body from URL %s: %v",url, err) 
-  }
+	if response.StatusCode != http.StatusOK {
+		return "", fmt.Errorf("Unexpected Status code: %d", response.StatusCode)
+	}
 
-  return string(bodyBytes), nil
+	bodyBytes, err := io.ReadAll(response.Body)
+	if err != nil {
+		return "", fmt.Errorf("An Error Occured while reading the body from URL %s: %v", url, err)
+	}
+
+	return string(bodyBytes), nil
 }
