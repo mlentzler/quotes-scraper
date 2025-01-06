34e5296966dbd640f6d7d6429477e3bf998df48e
mlentzler <michel.lentzler@gmail.com>
2025-01-06 17:51:12 +0100

    adds fetch function

--- internal/crawler/crawler.go
+++ internal/crawler/crawler.go
@@ -1,9 +1,33 @@
 package crawler
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
 
 
 
-func Crawl(link string) {
-  fmt.Printf("Crawling URL:%s\n", link)
+func Crawl(link string) (string, error){
+  return fetch(link)
+}
+
+func Fetch (url string) (string, error) {
+  response, err := http.Get(url)
+  if err != nil {
+    return "", fmt.Errorf("An Error Occured while fetching: %v", err)
+  }
+
+  defer response.Body.Close()
+
+  if response.StatusCode != http.StatusOK {
+    return "", fmt.Errorf("Unexpected Status code: %d", response.StatusCode)
+  }
+  
+  bodyBytes, err := io.ReadAll(response.Body)
+  if err != nil {
+    return "", fmt.Errorf("An Error Occured while reading the body from URL %s: %v",url, err) 
+  }
+
+  return string(bodyBytes), nil
 }
